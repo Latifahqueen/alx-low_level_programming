4ae1fc4134d33b3175b504a18f11b37ffe73522b
Latifahqueen <latifahnakato1@gmail.com>
2023-07-14 19:48:56 -0700

    parathesis

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -8,11 +8,13 @@
 */
 int main(void)
 {
+
 int n;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
+
 if (n > 0)
-}
+{
 printf("%d is positive\n", n);
 }
 else if (n == 0)
@@ -23,5 +25,6 @@ else
 {
 printf("%d is negative\n", n);
 }
+
 return (0);
 }
